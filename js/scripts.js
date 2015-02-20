1dc7db987035dceffa6edd74390b9b248aa65f10
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-20 11:39:03 -0800

    add website functionality, specs no longer passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
 var Pizza = {
-    pizzaProp: function ( size, type ) {
+    pizzaProps: function ( size, type ) {
       this.size = size,
       this.type = type;
     },
 
     pizzaSlice: function () {
-      if ( this.size > 14 && this.type === "cheese" ) {
+      if ( this.size > 14 && this.type === "Cheese" ) {
         return 10;
-      } else if ( this.size > 10 && this.type === "cheese") {
+      } else if ( this.size > 10 && this.type === "Cheese") {
         return 8;
-      } else if ( this.size > 6 && this.type === "cheese") {
+      } else if ( this.size > 6 && this.type === "Cheese") {
         return 6;
       }
+    },
 
+    pizzaValid: function () {
+      if ( this.size <= 5 || this.size > 20 ) {
+        return false;
+      }
     }
   }
+
+$(document).ready(function() {
+  $("form#new-pizza").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSize = parseInt($("input#size")).val();
+    var inputtedType = $("input#type").val();
+
+    var newPizza = Object.create(Pizza);
+    newPizza.pizzaProps(inputtedSize, inputtedType);
+
+    if (newPizza.pizzaValid()) {
+      var slices = newPizza.pizzaSlices();
+      $("#results h4").text("Your pizza is " + slices + " slices. Pig out!"  )
+      $("#results").show();
+    } else {
+      $("#invalid h4").text("Enter a valid size.")
+      $("#invalid").show()
+    }
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,9 @@
 describe("Pizza", function() {
 
-  describe("pizzaProp", function() {
+  describe("pizzaProps", function() {
     it("assigns the size and type of a pizza", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.pizzaProp(12, "cheese");
+      testPizza.pizzaProps(12, "cheese");
       expect(testPizza.size).to.equal(12);
     });
   });
@@ -11,8 +11,16 @@ describe("Pizza", function() {
   describe("pizzaSlice", function() {
     it("informs user how many slices based on size", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.pizzaProp(12, "cheese");
+      testPizza.pizzaProps(12, "cheese");
       expect(testPizza.pizzaSlice()).to.equal(8);
+    });
+  });
+
+  describe("pizzaValid", function() {
+    it("tells user if input is invalid", function() {
+      var testPizza = Object.create(Pizza);
+      testPizza.pizzaProps(5, "cheese");
+      expect(testPizza.pizzaValid()).to.equal(false);
     });
   });
 });
