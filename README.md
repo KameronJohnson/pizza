054a1283fb1277671125a43114f7164e96b227f6
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-20 14:40:41 -0800

    add README

---
+++ README.md
@@ -0,0 +1,25 @@
+Kam's Pizza Shoppe
+==============
+
+A JavaScript web app that allows a user to enter a desired pizza size and type, and  will return how many slices the pizza will yield based on the size.
+
+This app uses JQuery, Mocha, Chai and Bootstrap.
+
+Directions
+-----
+
+1) Open `pizza.html`
+
+2) Have fun.
+
+About
+-----
+
+By Kameron Johnson,
+
+JavaScript OO assessment at Epicodus, Winter 2015
+
+License
+-------
+
+MIT license.
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,7 @@
 describe("Pizza", function() {
 
   describe("pizzaProps", function() {
-    it("assigns the size and type of a pizza", function() {
+    it("assigns the size and type properties of a pizza", function() {
       var testPizza = Object.create(Pizza);
       testPizza.pizzaProps(12, "Pepperoni");
       expect(testPizza.size).to.equal(12);
@@ -9,7 +9,7 @@ describe("Pizza", function() {
   });
 
   describe("pizzaSlices", function() {
-    it("informs user how many slices based on size", function() {
+    it("informs user how many slices based on size of pizza", function() {
       var testPizza = Object.create(Pizza);
       testPizza.pizzaProps(9, "Pepperoni");
       expect(testPizza.pizzaSlices()).to.equal(6);
