6fc92888c1a7899113248e3fc56346e6e370d454
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-20 12:16:08 -0800

    add continued website functionality, specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,17 @@ var Pizza = {
     },
 
     pizzaSlice: function () {
-      if ( this.size > 14 && this.type === "Cheese" ) {
+      if ( this.size > 14 ) {
         return 10;
-      } else if ( this.size > 10 && this.type === "Cheese") {
+      } else if ( this.size >= 10 ) {
         return 8;
-      } else if ( this.size > 6 && this.type === "Cheese") {
+      } else if ( this.size >= 6 ) {
         return 6;
       }
     },
 
     pizzaValid: function () {
-      if ( this.size <= 5 || this.size > 20 ) {
+      if ( this.size < 6 || this.size > 20 ) {
         return false;
       }
     }
@@ -25,14 +25,14 @@ $(document).ready(function() {
   $("form#new-pizza").submit(function(event) {
     event.preventDefault();
 
-    var inputtedSize = parseInt($("input#size")).val();
+    var inputtedSize = parseInt($("input#input-size")).val();
     var inputtedType = $("input#type").val();
 
     var newPizza = Object.create(Pizza);
     newPizza.pizzaProps(inputtedSize, inputtedType);
 
     if (newPizza.pizzaValid()) {
-      var slices = newPizza.pizzaSlices();
+      var slices = newPizza.pizzaSlice();
       $("#results h4").text("Your pizza is " + slices + " slices. Pig out!"  )
       $("#results").show();
     } else {
--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,7 @@ describe("Pizza", function() {
   describe("pizzaProps", function() {
     it("assigns the size and type of a pizza", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.pizzaProps(12, "cheese");
+      testPizza.pizzaProps(12, "Cheese");
       expect(testPizza.size).to.equal(12);
     });
   });
@@ -11,15 +11,15 @@ describe("Pizza", function() {
   describe("pizzaSlice", function() {
     it("informs user how many slices based on size", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.pizzaProps(12, "cheese");
-      expect(testPizza.pizzaSlice()).to.equal(8);
+      testPizza.pizzaProps(9, "Cheese");
+      expect(testPizza.pizzaSlice()).to.equal(6);
     });
   });
 
   describe("pizzaValid", function() {
     it("tells user if input is invalid", function() {
       var testPizza = Object.create(Pizza);
-      testPizza.pizzaProps(5, "cheese");
+      testPizza.pizzaProps(5, "Cheese");
       expect(testPizza.pizzaValid()).to.equal(false);
     });
   });
