942f5e43ce158000db38a3d151a3ac9f6a3a0c84
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-20 14:04:45 -0800

    add pizza css

--- js/scripts.js
+++ js/scripts.js
@@ -30,15 +30,15 @@ $(document).ready(function() {
     var inputtedSize = parseInt($("input#input-size").val());
     var inputtedType = $("input#input-type").val();
 
-    // $("#input-size").val("");
+    $("#input-size").val("");
 
     var newPizza = Object.create(Pizza);
     newPizza.pizzaProps(inputtedSize, inputtedType);
 
     if (newPizza.pizzaValid()) {
       var slices = newPizza.pizzaSlice();
-      $("#results h4").text("Your pizza is " + slices + " slices. Pig out!" )
-      $("#results").show();
+      $("#show-pizza h4").text("Your pizza is " + slices + " slices. Pig out!" )
+      $("#show-pizza").show();
     } else {
       $("#invalid h4").text("Enter a valid size.")
       $("#invalid").show()
@@ -46,25 +46,3 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
-
-// $("#form-pizza").submit(function(event) {
-//   var inputtedSize = parseInt($("#input-size").val());
-//   var inputtedTopping = $("#input-topping").val();
-//   cleanPage();
-//   var pizza = Object.create(Pizza);
-//   pizza.init(inputtedSize, inputtedTopping);
-//   if (pizza.legit()) {
-//     var slices = pizza.numSlices();
-//     if(slices === 1) {
-//       $("#results h5").text("We don't cut tiny " + pizza.topping + " pizzas!");
-//     } else {
-//       $("#results h5").text("We cut your " + pizza.size + " inch " + pizza.topping + " pizza into " + slices + " slices.");
-//     }
-//     displayImg(slices, pizza.topping);
-//     $("#results").show();
-//   } else {
-//     $("#error h5").text("How do you expect us to slice a " + pizza.size + " inch pizza?!?");
-//     $("#error").show();
-//   }
-//
-//   event.preventDefault();
