36c3d6c79e819c29cb2fd93e5dfeffe1d2ea876f
Kameron Ross Johnson <KameronJohnson@gmail.com>
2015-02-20 12:40:33 -0800

    continue website functionality

--- js/scripts.js
+++ js/scripts.js
@@ -15,29 +15,56 @@ var Pizza = {
     },
 
     pizzaValid: function () {
-      if ( this.size < 6 || this.size > 20 ) {
+      if ( this.size < 6 || this.size > 22 ) {
         return false;
       }
+      return true;
     }
-  }
+  };
 
 $(document).ready(function() {
   $("form#new-pizza").submit(function(event) {
-    event.preventDefault();
 
-    var inputtedSize = parseInt($("input#input-size")).val();
-    var inputtedType = $("input#type").val();
+
+
+    var inputtedSize = parseInt($("input#input-size").val());
+    var inputtedType = $("input#input-type").val();
+
+    // $("#input-size").val("");
 
     var newPizza = Object.create(Pizza);
     newPizza.pizzaProps(inputtedSize, inputtedType);
 
     if (newPizza.pizzaValid()) {
       var slices = newPizza.pizzaSlice();
-      $("#results h4").text("Your pizza is " + slices + " slices. Pig out!"  )
+      $("#results h4").text("Your pizza is " + slices + " slices. Pig out!" )
       $("#results").show();
     } else {
       $("#invalid h4").text("Enter a valid size.")
       $("#invalid").show()
     }
+    event.preventDefault();
   });
 });
+
+// $("#form-pizza").submit(function(event) {
+//   var inputtedSize = parseInt($("#input-size").val());
+//   var inputtedTopping = $("#input-topping").val();
+//   cleanPage();
+//   var pizza = Object.create(Pizza);
+//   pizza.init(inputtedSize, inputtedTopping);
+//   if (pizza.legit()) {
+//     var slices = pizza.numSlices();
+//     if(slices === 1) {
+//       $("#results h5").text("We don't cut tiny " + pizza.topping + " pizzas!");
+//     } else {
+//       $("#results h5").text("We cut your " + pizza.size + " inch " + pizza.topping + " pizza into " + slices + " slices.");
+//     }
+//     displayImg(slices, pizza.topping);
+//     $("#results").show();
+//   } else {
+//     $("#error h5").text("How do you expect us to slice a " + pizza.size + " inch pizza?!?");
+//     $("#error").show();
+//   }
+//
+//   event.preventDefault();
